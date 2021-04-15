599d84864d2b26ea5e2e6c1eb7bf4eeb3f7ba944
alyaathirah <62088501+alyaathirah@users.noreply.github.com>
2021-04-15 15:04:00 +0800

    add sign up

--- js/login.js
+++ js/login.js
@@ -1,7 +1,21 @@
-function myFunction() {
-    alert("Hello! I am an alert box!");
+// Get the modal
+var modal = document.getElementById("myModal");
+
+
+// When the user clicks the button, open the modal 
+function openModal(){
+  //$(".big").css("border", "1px dashed orange");
+  $(".modal").css("display","block");
 }
 
-function buttonLogin(){
-    
+// When the user clicks on <span> (x), close the modal
+function closeModal() {
+  $(".modal").css("display","none");
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function closeWindow(event) {
+  if (event.target == modal) {
+    $(".modal").css("display","none");
+  }
 }
