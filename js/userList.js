c66a1e66275527be402279b1f83adff5832beed6
alyaathirah <62088501+alyaathirah@users.noreply.github.com>
2021-04-25 02:59:35 +0800

    add login

--- js/userList.js
+++ js/userList.js
@@ -1,20 +1,34 @@
-var email = document.getElementsByName("email");
-var password = document.getElementsByName("password");
 
-var user = [alya, mirul, hanani, mira, nurul, kina, marie]
+var user = ["alya", "mirul", "hanani", "mira", "nurul", "kina", "marie"];
 
-var userPass = [alya, mirul, hanani, mira, nurul, kina, marie]
+var userPass = ["alya", "mirul", "hanani", "mira", "nurul", "kina", "marie"];
 
-var admin = [admin, superadmin]
+var admin = ["admin", "superadmin"];
 
-var adminPass = [admin, superadmin]
+var adminPass = ["admin", "superadmin"];
 
-async function Login(email, password){
+function Login(){
+    var email = $("#email").val();
+    var password = $("#pass").val();  
+
+    //for admins only
     for(i=0; i<admin.length; i++){
         if(email == admin[i])
-            if(password = adminPass[i])
-                window.location.href("homepage.html")
+            if(password == adminPass[i]){
+                location.assign("homepage.html");//go to homepage admin
+                return;
+            }
     }
+
+    //for users only
+    for(i=0; i<user.length; i++){
+        if(email == user[i])
+            if(password == userPass[i]){
+                location.assign("homepage.html");//go to homepage user
+                return;
+            }
+    }
+    
 }
 
 function SignUp(){
