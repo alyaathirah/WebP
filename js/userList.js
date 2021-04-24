da491e717874e74bb993b07d65e74c2d41c4ac00
alyaathirah <62088501+alyaathirah@users.noreply.github.com>
2021-04-25 01:57:50 +0800

    add login

--- js/userList.js
+++ js/userList.js
@@ -1,3 +1,6 @@
+var email = document.getElementsByName("email");
+var password = document.getElementsByName("password");
+
 var user = [alya, mirul, hanani, mira, nurul, kina, marie]
 
 var userPass = [alya, mirul, hanani, mira, nurul, kina, marie]
@@ -5,3 +8,15 @@ var userPass = [alya, mirul, hanani, mira, nurul, kina, marie]
 var admin = [admin, superadmin]
 
 var adminPass = [admin, superadmin]
+
+async function Login(email, password){
+    for(i=0; i<admin.length; i++){
+        if(email == admin[i])
+            if(password = adminPass[i])
+                window.location.href("homepage.html")
+    }
+}
+
+function SignUp(){
+    
+}
