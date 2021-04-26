707074e169b64918ffca00d49578e88240e85f40
nuramirah <81424377+miramimo@users.noreply.github.com>
2021-04-27 00:21:04 +0800

    Add files via upload

---
+++ adminproduct.js
@@ -0,0 +1,76 @@
+ var category = localStorage.getItem("pdCategory");
+ item = document.getElementById("item1");
+        cloneditem = item.cloneNode(true);
+        cloneditem.id = 'item01';
+        cloneditem.querySelector("#iname1").innerHTML = localStorage.getItem("pdName");
+        cloneditem.querySelector("#inname1").value = localStorage.getItem("pdName");
+        cloneditem.querySelector("#iprice1").value = localStorage.getItem("pdPrice");
+        cloneditem.querySelector("#idesc1").innerHTML = localStorage.getItem("pdDes");
+        cloneditem.querySelector("#iweight1").value = localStorage.getItem("pdWeight");
+        cloneditem.querySelector("#istock1").value = localStorage.getItem("pdStock");
+        cloneditem.querySelector("#iexpDate1").value = localStorage.getItem("pdExpDate");
+        cloneditem.querySelector("#iimage1").src = localStorage.getItem("pdImage");
+
+        if(localStorage.getItem("pdLocal") != null){
+          cloneditem.querySelector("#ilocal1").checked = true;
+          cloneditem.querySelector("#iimported1").checked = false;
+        }else{
+          cloneditem.querySelector("#ilocal1").checked = false;
+          cloneditem.querySelector("#iimported1").checked = true;
+        }
+
+        var unit = localStorage.getItem("pdUnit");
+        if(unit == "kg"){
+          alert("kg");
+          cloneditem.querySelector("#iunit1").value = unit;
+        }else{
+          cloneditem.querySelector("#iunit1").value = "g";
+        }
+
+
+    
+      if(category=="Snacks"){
+        //SNACKS   
+        document.getElementById("snacks_items1").appendChild(cloneditem);
+        console.log(cloneditem);
+      }else if(category=="Fruit and Vegetables"){
+        //FRUIT AND VEG
+        document.getElementById("fruitnvegs_items1").appendChild(cloneditem);
+        console.log(cloneditem);
+      }else if(category=="Instant Food"){
+        //Instant Food
+        document.getElementById("instantfoods_items1").appendChild(cloneditem);
+        console.log(cloneditem);
+      }
+
+      //collapse desc
+ var coll = document.getElementsByClassName("collapsible");
+ var i;
+ 
+ for (i = 0; i < coll.length; i++) {
+   coll[i].addEventListener("click", function() {
+     this.classList.toggle("descactive");
+     var content = this.nextElementSibling;
+     if (content.style.display === "block") {
+       content.style.display = "none";
+     } else {
+       content.style.display = "block";
+     }
+   });
+ }
+
+ function delFunction() {
+  var id = localStorage.getItem("iddel");
+  var buttonID = "item" + id;
+  var myobj = document.getElementById(buttonID);
+  myobj.remove();
+  localStorage.removeItem('iddel');
+}
+
+function idFunction(){
+localStorage.setItem("iddel",event.srcElement.id);
+}
+
+function del(){
+window.localStorage.clear();
+}
